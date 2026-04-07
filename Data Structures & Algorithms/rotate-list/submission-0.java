b04951bbbc72766f8855c3b60057e3313ca69f4e
Satyam Kumar <satyamkumar2351056@gmail.com>
2026-04-07 16:52:21 +0530

    Add: rotate-list - submission-0

---
+++ Data Structures & Algorithms/rotate-list/submission-0.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k == 0) return head;
+
+        ListNode temp=head;
+        int len=1;
+        while(temp.next != null) {
+            temp = temp.next;
+            len++;
+        }
+
+        temp.next=head;
+        k = k % len;
+        int steps = len - k;
+        ListNode newTail = head;
+
+        for(int i = 1; i < steps; i++) {
+            newTail = newTail.next;
+        }
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
